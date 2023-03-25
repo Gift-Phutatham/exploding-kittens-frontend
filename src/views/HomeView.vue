807af1d15b1a62103925cef9e7104da6b3aa2378
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2023-03-25 18:46:58 +0700

    decorate

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-image d-flex align-end justify-center">
-    <v-btn class="button">Create / Join</v-btn>
+    <v-btn class="button">Create / Join Room</v-btn>
   </div>
 </template>
 

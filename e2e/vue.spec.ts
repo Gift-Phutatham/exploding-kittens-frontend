ff9b6c60f7ee99be5b4b8a17286030dd8b5358f1
Andrew-Stuber <andrewstuber20@gmail.com>
2023-03-28 00:43:48 +0700

    routing and 404 page

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -2,7 +2,22 @@ import { test, expect } from '@playwright/test';
 
 // See here how to get started:
 // https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
+test('visits the home page root url', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
+  await page.locator('button').click({ force: true });
+});
+
+test('visits the game room root url', async ({ page }) => {
+  await page.goto('/GameRoom');
+  await expect(page.locator('h1')).toHaveText('Game Room');
+});
+
+test('visits the create room root url', async ({ page }) => {
+  await page.goto('/CreateRoom');
+  await expect(page.locator('h1')).toHaveText('Create Room');
+});
+
+test('visits the 404 root url', async ({ page }) => {
+  await page.goto('/404');
+  await expect(page.locator('h1')).toHaveText('404');
 });
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1 class="text1">404</h1>
+    <h2 class="text2">Page Not Found</h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.text1 {
+  text-align: center;
+}
+.text2 {
+  text-align: center;
+}
+</style>

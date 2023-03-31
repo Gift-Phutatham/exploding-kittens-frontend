a55c43ad0c5ce22daa4e11a57aab292dbf1715d5
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2023-03-31 11:04:28 +0700

    add card component

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps<{
+  name: string;
+  description: string;
+  color: string;
+}>();
+</script>
+
+<template>
+  <v-card class="mx-auto" max-width="200" variant="outlined">
+    <v-container>
+      <v-card variant="outlined" :color="color">
+        <v-card-item>
+          <div class="text-center text-grey-darken-3 my-10">
+            <div class="text-h6">
+              {{ name }}
+            </div>
+            <v-divider class="border-opacity-100 my-5" :color="color"></v-divider>
+            <div class="text-caption">
+              {{ description }}
+            </div>
+          </div>
+        </v-card-item>
+      </v-card>
+    </v-container>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CardComponent',
+});
+</script>

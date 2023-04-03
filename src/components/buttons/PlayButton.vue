6d658560cb789088cd3ded88f9e29092e856da57
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2023-04-03 16:57:17 +0700

    add buttons

---
+++ src/components/buttons/PlayButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn prepend-icon="mdi-cards" color="success"> Play </v-btn>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PlayButton',
+});
+</script>

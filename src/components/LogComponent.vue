a36dc2e00edcd3a3ff7058b2b2de844105c2988c
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2023-04-03 19:23:46 +0700

    decorate

--- src/components/LogComponent.vue
+++ src/components/LogComponent.vue
@@ -1,9 +1,8 @@
 <template>
-  <v-card class="mx-auto overflow-y-auto" max-height="400" max-width="250">
-    <v-banner class="text-center text-subtitle-1 py-2" sticky> Game Log </v-banner>
-    <v-divider></v-divider>
-    <v-card-item>
-      <div class="text-justify text-caption my-1" v-for="(message, index) in messages" :key="index">
+  <v-card class="mx-auto overflow-y-auto" height="400" max-width="250">
+    <v-banner class="text-center text-subtitle-1 py-2 banner" sticky> Game Log </v-banner>
+    <v-card-item class="text-justify text-caption">
+      <div class="my-1" v-for="(message, index) in messages" :key="index">
         {{ message }}
         <v-divider v-if="index != messages.length - 1"></v-divider>
       </div>
@@ -21,3 +20,9 @@ export default defineComponent({
   }),
 });
 </script>
+
+<style scoped>
+.banner {
+  z-index: 1;
+}
+</style>

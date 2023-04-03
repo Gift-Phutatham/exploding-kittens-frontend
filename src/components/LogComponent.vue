3852876c72bdb985abf85b658e61d18afe77c7e6
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2023-04-03 19:42:19 +0700

    decorate

--- src/components/LogComponent.vue
+++ src/components/LogComponent.vue
@@ -1,12 +1,14 @@
 <template>
-  <v-card class="mx-auto overflow-y-auto" height="400" max-width="250">
-    <v-banner class="text-center text-subtitle-1 py-2 banner" sticky> Game Log </v-banner>
-    <v-card-item class="text-justify text-caption">
-      <div class="my-1" v-for="(message, index) in messages" :key="index">
-        {{ message }}
-        <v-divider v-if="index != messages.length - 1"></v-divider>
-      </div>
-    </v-card-item>
+  <v-card class="mx-auto" max-width="250">
+    <v-banner class="text-center text-subtitle-1 py-2"> Game Log </v-banner>
+    <v-card class="overflow-y-auto" height="300">
+      <v-card-item class="text-justify text-caption">
+        <div class="my-1" v-for="(message, index) in messages" :key="index">
+          {{ message }}
+          <v-divider v-if="index != messages.length - 1"></v-divider>
+        </div>
+      </v-card-item>
+    </v-card>
   </v-card>
 </template>
 
@@ -16,13 +18,7 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'LogComponent',
   data: () => ({
-    messages: ['Exploding Kittens has started'],
+    messages: [],
   }),
 });
 </script>
-
-<style scoped>
-.banner {
-  z-index: 1;
-}
-</style>

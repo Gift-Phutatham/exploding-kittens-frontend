735bb37998ca9e390a5d0505a25c3dcdb985c240
marcArisen <markjingnum9@gmail.com>
2023-04-09 22:01:06 +0700

    add socketservice

--- src/services/socketio.service.ts
+++ src/services/socketio.service.ts
@@ -4,8 +4,37 @@ class SocketioService {
   socket: any;
   constructor() {}
 
-  setupSocketConnection() {
-    this.socket = io('http://localhost:3000');
+  setupSocketConnection(token: any) {
+    this.socket = io('http://localhost:3000', {
+      // TODO: Fix this hardcade port address
+      auth: {
+        token,
+      },
+    });
+    console.log(`Connecting socket...`);
+  }
+
+  subscribeToMessages(roomID: string) {
+    if (!this.socket) return true;
+    this.socket.on('message', (msg: any) => {
+      console.log('Room event received!');
+    });
+  }
+
+  subscribeToRoom(roomID: string) {
+    if (!this.socket) return true;
+    this.socket.emit('join room', roomID); // join the specific room
+    this.socket.on('join room', (msg: any) => {
+      console.log('Room event received!');
+    });
+  }
+
+  sendMessage(message: any) {
+    if (this.socket) this.socket.emit('message', message);
+  }
+
+  startGame() {
+    if (this.socket) this.socket.emit('game loop');
   }
 
   disconnect() {

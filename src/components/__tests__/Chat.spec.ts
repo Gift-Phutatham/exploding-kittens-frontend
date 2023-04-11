04b824bfb4701df85e4fd2dc55a9785971838e66
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2023-04-12 01:41:11 +0700

    checkpoint

--- src/components/__tests__/Chat.spec.ts
+++ src/components/__tests__/Chat.spec.ts
@@ -1,44 +1,42 @@
-import { describe, it, expect} from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import { createVuetify } from 'vuetify';
 import ChatComponent from '../ChatComponent.vue';
 import vuetify from '@/plugins/vuetify';
 
 describe('ChatComponent', () => {
-    const vuetify = createVuetify();
-    const wrapper = mount(ChatComponent, {
-        global: {
-            plugins: [vuetify],
-        },
+  const vuetify = createVuetify();
+  const wrapper = mount(ChatComponent, {
+    global: {
+      plugins: [vuetify],
+    },
+  });
+
+  it('render component', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('adds a new chat message', async () => {
+    const message = wrapper.find('input[type="text"]');
+    const send = wrapper.find('.v-input__icon--append-inner');
+
+    await message.setValue('Hello');
+    await send.trigger('click');
+
+    expect(wrapper.vm.chats).to.deep.equal({
+      name: 'Anonymous',
+      message: 'Hello',
     });
+  });
 
-    it('render component', () => {
-        expect(wrapper.exists()).toBe(true);
-    });
+  // it('send a message to the chat box', async () => {
+  //     const message = wrapper.find('input[type=text]');
+  //     const send = wrapper.find('button');
 
-    it('adds a new chat message', async () => {
-        const message = wrapper.find('input[type="text"]');
-        const send = wrapper.find('.v-input__icon--append-inner');
+  //     message.setValue('Hello');
+  //     await send.trigger('click');
 
-        await message.setValue('Hello');
-        await send.trigger('click');
-    
-
-        expect(wrapper.vm.chats).to.deep.equal({
-            name: 'Anonymous',
-            message: 'Hello',
-        });
-    });
-
-
-    // it('send a message to the chat box', async () => {
-    //     const message = wrapper.find('input[type=text]');
-    //     const send = wrapper.find('button');
-
-    //     message.setValue('Hello');
-    //     await send.trigger('click');
-
-    //     expect(wrapper.findAll('.text-caption').length).toBe(1);
-    //     expect(wrapper.find('.text-caption div').text()).toBe('Hello');
-    // });
+  //     expect(wrapper.findAll('.text-caption').length).toBe(1);
+  //     expect(wrapper.find('.text-caption div').text()).toBe('Hello');
+  // });
 });
--- src/components/__tests__/DrawPile.spec.ts
+++ src/components/__tests__/DrawPile.spec.ts
@@ -1,29 +1,27 @@
-import { describe, it, expect} from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import { createVuetify } from 'vuetify';
 import DrawPileComponent from '../DrawPileComponent.vue';
-import img from '@/assets/images/ExplodingKittens.png'
+import img from '@/assets/images/ExplodingKittens.png';
 
 describe('DrawPileComponent', () => {
-    const vuetify = createVuetify();
-    const wrapper = mount(DrawPileComponent, {
-        global: {
-            plugins: [vuetify],
-        },
-    });
+  const vuetify = createVuetify();
+  const wrapper = mount(DrawPileComponent, {
+    global: {
+      plugins: [vuetify],
+    },
+  });
 
-    it('renders the draw pile', () => {
-        expect(wrapper.exists()).toBe(true);
-    });
+  it('renders the draw pile', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
 
-    it('renders the image', async () => {
+  it('renders the image', async () => {
+    const image = wrapper.find('.v-img');
+    expect(image.exists()).toBe(true);
+  });
 
-        const image = wrapper.find('.v-img');
-        expect(image.exists()).toBe(true);
-
-    });
-
-    it('display the title', () => {
-        expect(wrapper.find('.text-center').text()).toContain('Exploding');
-    });
+  it('display the title', () => {
+    expect(wrapper.find('.text-center').text()).toContain('Exploding');
+  });
 });

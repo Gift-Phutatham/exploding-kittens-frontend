1fa31a7c9f52e0971bfeb91246255d690a16ea72
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2023-04-11 21:19:25 +0700

    checkpoint

--- src/App.vue
+++ src/App.vue
@@ -31,3 +31,31 @@ export default {
   opacity: 90%;
 }
 </style>
+
+<!-- <template>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+.bg-image {
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  opacity: 90%;
+}
+</style> -->
